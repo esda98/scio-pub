59e4ca3506487f61c79fe8b7fed3d0b889a50cbc
Erik Abramczyk <esda98@gmail.com>
2020-09-14 20:05:21 -0400

    Added a way to add new employees to the Tree
    Made more employees in the initial tree

--- src/views/TreeDemo.vue
+++ src/views/TreeDemo.vue
@@ -3,21 +3,55 @@
     <v-row>
       <v-col cols="12" class="pt-0">
         <v-row>
-          <v-col cols="4" class="py-1">
-            <v-btn style="width: 100%">Reset</v-btn>
+          <v-col cols="3" class="py-0 px-1 pl-2">
+            <v-btn :disabled="selectedIndexes.length === 0" style="width: 100%" @click="showAddDialog = true">Add To Selected</v-btn>
           </v-col>
-          <v-col cols="4" class="py-1">
+          <v-col cols="3" class="py-0 px-1">
+            <v-btn style="width: 100%" @click="reset">Reset</v-btn>
+          </v-col>
+          <v-col cols="3" class="py-0 px-1">
+            <v-btn style="width: 100%">Sort</v-btn>
+          </v-col>
+          <v-col cols="3" class="py-0 px-1 pr-2">
             <v-btn style="width: 100%">Load</v-btn>
           </v-col>
-          <v-col cols="4" class="py-1">
-            <v-btn style="width: 100%">tootles</v-btn>
-          </v-col>
+
         </v-row>
       </v-col>
     </v-row>
     <v-row>
-      <v-treeview style="width: 100%" :items="rootNodes" item-children="children" item-key="name" item-text="displayLabel" activatable></v-treeview>
+      <v-treeview style="width: 100%" :items="rootNodes" item-children="children" item-key="name" item-text="displayLabel" activatable :active.sync="selectedIndexes"></v-treeview>
     </v-row>
+    <v-dialog v-model="showAddDialog" max-width="500">
+      <v-card>
+        <v-card-title>New Employee</v-card-title>
+        <v-card-text>
+          <v-form ref="newEmployeeForm" v-model="addValid">
+            <v-text-field v-model="addFirstName" placeholder="First Name" clearable :rules="textRules"></v-text-field>
+            <v-text-field v-model="addLastName" placeholder="Last Name" clearable :rules="textRules"></v-text-field>
+            <v-text-field v-model="addJobTitle" placeholder="Job Title" clearable :rules="textRules"></v-text-field>
+          </v-form>
+          <div v-if="showNotUniqueMessage" style="color: red">This Employee Already Exists</div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" @click="addEmployee">Add</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-snackbar
+        v-model="showAddedBar"
+        :timeout="1500">
+      Added Child!
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            color="blue"
+            text
+            v-bind="attrs"
+            @click="showCopiedTextNotif = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-container>
 </template>
 
@@ -30,14 +64,76 @@ export default Vue.extend({
   name: "TreeDemo",
   data: () => {
     return {
-      rootNodes: [] as Tree<Employee>[]
+      rootNodes: [] as Tree<Employee>[],
+      showAddDialog: false,
+      addFirstName: "",
+      addLastName: "",
+      addJobTitle: "",
+      selectedIndexes: [],
+      textRules: [
+          (v: string) => !!v || 'Must have a value'
+      ],
+      addValid: false,
+      showNotUniqueMessage: false,
+      showAddedBar: false
     }
   },
   mounted() {
-    const root1 = new Tree(new Employee("Billy", "Bob", "CTO"));
-    this.rootNodes.push(root1)
-    //add a child to test
-    root1.addChild(new Employee("Scott", "Smith", "Project Lead"))
+    this.reset()
+  },
+  methods: {
+    reset() {
+      const ceo = new Tree(new Employee("Billy", "Bob", "CEO"));
+      const founder1 = new Tree(new Employee("Bobby", "Brown", "Co-Founder"))
+      const founder2 = new Tree(new Employee("Francis", "Scott", "Co-Founder"))
+      this.rootNodes = [ceo, founder1, founder2]
+      //add a child to test
+      ceo.addChild(new Employee("Scott", "Smith", "CTO"))
+    },
+    addEmployee() {
+      this.showNotUniqueMessage = false;
+      console.log('Add: ', this.addFirstName, this.addLastName, this.addJobTitle);
+      console.log('Add to: ', this.selectedIndexes);
+      // eslint-disable-next-line
+      //@ts-ignore
+      this.$refs.newEmployeeForm.validate();
+      if(!this.addValid) { return; }
+      //build the object for the entered values
+      const newEmployeeObj = new Employee(this.addFirstName, this.addLastName, this.addJobTitle);
+      const newEmployeeName = newEmployeeObj.getName()
+      for(const r of this.rootNodes) {
+        if (r.findByName(newEmployeeName)) {
+          this.showNotUniqueMessage = true;
+          return;
+        }
+      }
+      //new employee is unique, lets add them!
+      //find selected hierarchy
+      if (this.selectedIndexes.length != 1) { return; }
+      const selectedName = this.selectedIndexes[0];
+      let selectedTree: Tree<Employee> | null = null;
+      for (const r of this.rootNodes) {
+        const res = r.findByName(selectedName)
+        if (res) {
+          selectedTree = res
+          break;
+        }
+      }
+      if (!selectedTree) { return; }
+      //got the selected tree, add this as a child
+      selectedTree.addChild(newEmployeeObj);
+      this.showAddDialog = false;
+      this.showAddedBar = true;
+    }
+  },
+  watch: {
+    showAddDialog (to) {
+      if (to) {
+        this.addJobTitle = "";
+        this.addFirstName = "";
+        this.addLastName = "";
+      }
+    }
   }
 })
 </script>
